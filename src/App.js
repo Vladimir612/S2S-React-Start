7e27acb561a40e746c1e7b0258ae181f57e5b588
Nikola Mirilo <nikolamirilo@gmail.com>
2021-10-24 19:35:01 +0200

    vraceno na pocetna podesavanja

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import "./App.css";
 import "./assets/css/fonts.css";
-import { Switch, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import RoutePage from "./Pages/RoutePage";
-import Error from "./Pages/Error";
 
 const App = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/players/:player" component={RoutePage} />
-      <Route exact path="/teams/:team" component={RoutePage} />
-      <Route path="*" component={Error} />
-    </Switch>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,28 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { SinglePlayerProvider } from "./data/SinglePlayerContext";
-import { SingleTeamProvider } from "./data/SingleTeamContext";
-import { GamesProvider } from "./data/GamesContext";
-import { TeamsProvider } from "./data/TeamsContext";
-import { PlayersProvider } from "./data/PlayersContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <SinglePlayerProvider>
-        <SingleTeamProvider>
-          <GamesProvider>
-            <TeamsProvider>
-              <PlayersProvider>
-                <App />
-              </PlayersProvider>
-            </TeamsProvider>
-          </GamesProvider>
-        </SingleTeamProvider>
-      </SinglePlayerProvider>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
